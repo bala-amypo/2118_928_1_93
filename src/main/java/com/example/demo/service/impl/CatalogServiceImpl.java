56a7855094f20506d584716534524720fec3e2e0
bala-amypo <bala@amypo.in>
2025-12-18 09:08:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -1,2 +1,6 @@
 package com.example.demo.service.inpl;
-import com.example.demo.entity.ActiveIn
+import com.example.demo.entity.ActiveIngredient;
+import com.example.demo.repo.ActiveIngredientRepository;
+import com.example.deo.repo.MedicationRepository;
+import com.example.demo.service.CatalogService;
+import org.springframework.stereotype
