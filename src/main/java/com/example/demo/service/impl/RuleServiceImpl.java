07bd2a8bbf9c26f360a814b8ca1529b0c6aec129
bala-amypo <bala@amypo.in>
2025-12-18 15:18:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RuleServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.InteractionRuleRepository;
+import com.example.demo.service.RuleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RuleServiceImpl implements RuleService {
+
+    public RuleServiceImpl(InteractionRuleRepository r) {
+    }
+}
