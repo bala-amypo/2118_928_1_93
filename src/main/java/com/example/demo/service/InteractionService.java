19633d6f744eab8362d09455136cafbb56bab465
bala-amypo <bala@amypo.in>
2025-12-18 08:52:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/InteractionService.java
@@ -0,0 +1,7 @@
+package.com.example.demo.service;
+import com.example.demo.entity.InteractionCheckResult;
+import java.util.List;
+public interface InteractionService{
+    InteractionCheckResult checkInteractions(List<Long>medicationIds);
+    InteractionCheckResult getResult(Long resultId);
+}
