cc0d9a9bde68bc05ab857b5c95c6c980e82101f4
bala-amypo <bala@amypo.in>
2025-12-18 15:19:36 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,34 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.ActiveIngredient;
-import com.example.demo.entity.Medication;
-import com.example.demo.service.CatalogService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/catalog")
 public class CatalogController {
-
-    private final CatalogService catalogService;
-
-    public CatalogController(CatalogService catalogService) {
-        this.catalogService = catalogService;
-    }
-
-    @PostMapping("/ingredient")
-    public ActiveIngredient addIngredient(@RequestBody ActiveIngredient ingredient) {
-        return catalogService.addIngredient(ingredient);
-    }
-
-    @PostMapping("/medication")
-    public Medication addMedication(@RequestBody Medication medication) {
-        return catalogService.addMedication(medication);
-    }
-
-    @GetMapping("/medications")
-    public List<Medication> getAll() {
-        return catalogService.getAllMedications();
-    }
 }
