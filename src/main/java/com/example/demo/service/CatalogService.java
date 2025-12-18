e9ee40d68c1b3e829e1c62e5322990e3e4df5672
bala-amypo <bala@amypo.in>
2025-12-18 08:45:54 +0000

    User commit

--- src/main/java/com/example/demo/service/CatalogService.java
+++ src/main/java/com/example/demo/service/CatalogService.java
@@ -1,2 +1,9 @@
 package com.example.demo.service;
 import com.example.demo.entity.ActiveIntegredient;
+import com.example.demo.entity.Medication;
+import java.util.List;
+public interface CatalogService{
+    ActiveIngredient addIntegredient(ActiveIngredient ingredient);
+    Medication addMedication(Medication medication);
+    List<Medication>getAllMedication();
+}
