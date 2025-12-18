df6d777bfda1a17c29d89a863ece55f16d5e2139
bala-amypo <bala@amypo.in>
2025-12-18 09:12:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -3,4 +3,9 @@ import com.example.demo.entity.ActiveIngredient;
 import com.example.demo.repo.ActiveIngredientRepository;
 import com.example.deo.repo.MedicationRepository;
 import com.example.demo.service.CatalogService;
-import org.springframework.stereotype
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class CatalogServiceImpl implements CatalogService{
+    private
+}
