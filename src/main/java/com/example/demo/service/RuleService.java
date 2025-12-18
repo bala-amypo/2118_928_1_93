af9a5866796373a69d24e2ac36c173bdb36c3030
bala-amypo <bala@amypo.in>
2025-12-18 15:17:11 +0000

    User commit

--- src/main/java/com/example/demo/service/RuleService.java
+++ src/main/java/com/example/demo/service/RuleService.java
@@ -1,11 +1,4 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.InteractionRule;
-import java.util.List;
-
 public interface RuleService {
-
-    InteractionRule addRule(InteractionRule rule);
-
-    List<InteractionRule> getAllRules();
 }
