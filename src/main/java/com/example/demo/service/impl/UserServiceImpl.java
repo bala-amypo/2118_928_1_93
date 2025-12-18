8219032e2fe598f69ee216b8de1a02041b705070
bala-amypo <bala@amypo.in>
2025-12-18 08:58:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,2 +1,16 @@
 package com.example.demo.service.impl;
-import com.example.demo.entity
+import com.example.demo.entity.User;
+import com.example.demo.repo.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+@Service
+public class UserServiceIpl implements UserService{
+    private final UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+    @Override
+    public User register(Usrr user){
+        return 
+    }
+}
