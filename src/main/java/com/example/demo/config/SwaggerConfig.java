b0b220888d9f557980b603f8799718db2295df5b
bala-amypo <bala@amypo.in>
2025-12-19 05:17:17 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,26 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Drug Interaction Checker API")
+                        .description("Swagger CRUD APIs")
+                        .version("1.0"))
+                
                 .servers(List.of(
-                        new Server().url("https://9056.pro604cr.amypo.ai/")
+                        new Server().url("/")
                 ));
-        }
+    }
 }
