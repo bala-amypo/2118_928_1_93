9ef197477a8b4f0946da9c4ef8351d46df946e06
bala-amypo <bala@amypo.in>
2025-12-19 16:34:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RuleServiceImpl.java
@@ -1,36 +1,33 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.InteractionRule;
+import com.example.demo.repository.InteractionRuleRepository;
 import com.example.demo.service.RuleService;
-import com.example.demo.service.Validation;
+import com.example.demo.service.validation.ValidationService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RuleServiceImpl implements RuleService {
 
-    private final Validation validation;
+    private final InteractionRuleRepository ruleRepository;
+    private final ValidationService validationService;
 
-    // temporary in-memory storage
-    private final List<InteractionRule> rules = new ArrayList<>();
-
-    public RuleServiceImpl(Validation validation) {
-        this.validation = validation;
+    public RuleServiceImpl(InteractionRuleRepository ruleRepository,
+                           ValidationService validationService) {
+        this.ruleRepository = ruleRepository;
+        this.validationService = validationService;
     }
 
     @Override
     public InteractionRule addRule(InteractionRule rule) {
-
-        validation.validateRule(rule);
-
-        rules.add(rule);
-        return rule;
+        validationService.validateRule(rule);
+        return ruleRepository.save(rule);
     }
 
     @Override
     public List<InteractionRule> getAllRules() {
-        return rules;
+        return ruleRepository.findAll();
     }
 }
