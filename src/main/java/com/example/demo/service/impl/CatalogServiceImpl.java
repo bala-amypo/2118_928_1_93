6a2c5685112beed33c40091483fe2740fa2aafba
bala-amypo <bala@amypo.in>
2025-12-20 05:06:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CatalogServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.ActiveIngredient;
+import com.example.demo.entity.Medication;
+import com.example.demo.repository.ActiveIngredientRepository;
+import com.example.demo.repository.MedicationRepository;
+import com.example.demo.service.CatalogService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CatalogServiceImpl implements CatalogService {
+
+    private final MedicationRepository medicationRepository;
+    private final ActiveIngredientRepository activeIngredientRepository;
+
+    public CatalogServiceImpl(MedicationRepository medicationRepository,
+                              ActiveIngredientRepository activeIngredientRepository) {
+        this.medicationRepository = medicationRepository;
+        this.activeIngredientRepository = activeIngredientRepository;
+    }
+
+    @Override
+    public ActiveIngredient addIngredient(ActiveIngredient ingredient) {
+        return activeIngredientRepository.save(ingredient);
+    }
+
+    @Override
+    public Medication addMedication(Medication medication) {
+
+        Set<ActiveIngredient> managedIngredients = new HashSet<>();
+
+        for (ActiveIngredient ing : medication.getIngredients()) {
+            managedIngredients.add(
+                activeIngredientRepository.findById(ing.getId()).orElseThrow()
+            );
+        }
+
+        medication.setIngredients(managedIngredients);
+        return medicationRepository.save(medication);
+    }
+
+    @Override
+    public List<Medication> getAllMedications() {
+        return medicationRepository.findAll();
+    }
+}
