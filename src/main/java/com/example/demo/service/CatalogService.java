ddfcf5826a17186f518db33fde6fa59c7d6cabb6
bala-amypo <bala@amypo.in>
2025-12-24 15:01:48 +0000

    User commit

--- src/main/java/com/example/demo/service/CatalogService.java
+++ src/main/java/com/example/demo/service/CatalogService.java
@@ -1,4 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.model.ActiveIngredient;
+import com.example.demo.model.Medication;
+import java.util.List;
+
 public interface CatalogService {
+
+    void addIngredient(ActiveIngredient ingredient);
+
+    void addMedication(Medication medication);
+
+    List<Medication> getAllMedications();
 }
--- src/main/java/com/example/demo/service/RuleService.java
+++ src/main/java/com/example/demo/service/RuleService.java
@@ -1,4 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.model.InteractionRule;
+import java.util.List;
+
 public interface RuleService {
+
+    void addRule(InteractionRule rule);
+
+    List<InteractionRule> getAllRules();
 }
