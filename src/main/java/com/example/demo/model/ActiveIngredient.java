de397dbffb6b478eeafc0ad82eeaeeea57962f39
bala-amypo <bala@amypo.in>
2025-12-24 05:12:33 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -11,21 +11,19 @@ public class ActiveIngredient {
 
     private String name;
 
-    // REQUIRED
-    public ActiveIngredient() {
-    }
-
-    // REQUIRED by TEST
-    public ActiveIngredient(String name) {
-        this.name = name;
-    }
-
-    // ===== Getters & Setters =====
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/com/example/demo/model/InteractionRule.java
+++ src/main/java/com/example/demo/model/InteractionRule.java
@@ -10,15 +10,51 @@ public class InteractionRule {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "ingredient1_id")
-    private Ingredient ingredient1;
+    private ActiveIngredient ingredient1;
 
     @ManyToOne
-    @JoinColumn(name = "ingredient2_id")
-    private Ingredient ingredient2;
+    private ActiveIngredient ingredient2;
 
     private String severity;
     private String description;
 
-    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ActiveIngredient getIngredient1() {
+        return ingredient1;
+    }
+
+    public void setIngredient1(ActiveIngredient ingredient1) {
+        this.ingredient1 = ingredient1;
+    }
+
+    public ActiveIngredient getIngredient2() {
+        return ingredient2;
+    }
+
+    public void setIngredient2(ActiveIngredient ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
