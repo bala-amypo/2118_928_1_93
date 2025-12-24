bc54b7844dce3ed9b8299f94313dbec2dc03a90d
bala-amypo <bala@amypo.in>
2025-12-24 04:12:32 +0000

    User commit

--- src/main/java/com/example/demo/model/ActiveIngredient.java
+++ src/main/java/com/example/demo/model/ActiveIngredient.java
@@ -1,5 +1,4 @@
-package com.example.demo.entity;
-
+package com.example.demo.model;
 
 import jakarta.persistence.*;
 
--- src/main/java/com/example/demo/model/Medication.java
+++ src/main/java/com/example/demo/model/Medication.java
@@ -1,4 +1,5 @@
-package com.example.demo.entity;
+package com.example.demo.model;
+
 
 
 import jakarta.persistence.*;
