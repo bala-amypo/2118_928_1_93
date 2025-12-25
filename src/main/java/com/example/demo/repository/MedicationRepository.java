55e49b616300cb910a4efba3479ab8401675a800
bala-amypo <bala@amypo.in>
2025-12-25 04:18:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/MedicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Medication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicationRepository extends JpaRepository<Medication, Long> {
-}
