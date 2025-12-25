1928420a39d93e4b904b021d4105203d73c49eeb
bala-amypo <bala@amypo.in>
2025-12-25 04:53:28 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,39 +1,15 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
-
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        final String securitySchemeName = "Bearer Authentication";
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Drug Interaction Checker API")
-                        .description("Spring Boot REST API for checking drug interactions")
-                        .version("1.0"))
-                .addSecurityItem(
-                        new SecurityRequirement().addList(securitySchemeName)
-                )
-                .components(
-                        new Components()
-                                .addSecuritySchemes(securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                )
-                );
-    }
-}
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Drug Interaction Checker API",
+        version = "1.0",
+        description = "Swagger for Drug Checker"
+    )
+)
+public class SwaggerConfig {}
