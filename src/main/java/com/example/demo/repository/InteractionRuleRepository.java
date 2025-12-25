2b8e11840ae48745257f70397f5848b0900d5187
bala-amypo <bala@amypo.in>
2025-12-25 05:25:46 +0000

    User commit

--- src/main/java/com/example/demo/repository/InteractionRuleRepository.java
+++ src/main/java/com/example/demo/repository/InteractionRuleRepository.java
@@ -2,11 +2,20 @@ package com.example.demo.repository;
 
 import com.example.demo.model.InteractionRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface InteractionRuleRepository
-        extends JpaRepository<InteractionRule, Long> {
+public interface InteractionRuleRepository extends JpaRepository<InteractionRule, Long> {
 
-    Optional<InteractionRule> findRuleBetweenIngredients(Long id1, Long id2);
+    @Query("""
+        SELECT r FROM InteractionRule r
+        WHERE (r.ingredientA.id = :id1 AND r.ingredientB.id = :id2)
+           OR (r.ingredientA.id = :id2 AND r.ingredientB.id = :id1)
+    """)
+    Optional<InteractionRule> findRuleBetweenIngredients(
+            @Param("id1") Long id1,
+            @Param("id2") Long id2
+    );
 }
